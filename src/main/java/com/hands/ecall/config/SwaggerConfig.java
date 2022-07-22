5d6d330d94b4f8ed77d98dc46c9de595e59ade44
uuxxi X <2933171792@qq.com>
2022-07-22 11:36:15 +0800

    逻辑迁移到service层

---
+++ src/main/java/com/hands/ecall/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.hands.ecall.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+/**
+ * @Author: Xuuxxi
+ * @Date: 2022/7/22
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean //配置docket以配置Swagger具体参数
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .enable(true); //改为false则无法访问
+    }
+
+    private ApiInfo apiInfo() {
+        Contact contact = new Contact("联系人名字", "https://www.4399.com", "联系人邮箱");
+        return new ApiInfo(
+                "ECALL", // 标题
+                "ECALL接口文档", // 描述
+                "v1.0", // 版本
+                "https://www.4399.com", // 组织链接
+                contact, // 联系人信息
+                "Apach 2.0 许可", // 许可
+                "许可链接", // 许可连接
+                new ArrayList<>()// 扩展
+        );
+    }
+}
--- src/main/java/com/hands/ecall/config/WebMvcConfig.java
+++ src/main/java/com/hands/ecall/config/WebMvcConfig.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import java.util.List;
@@ -20,6 +21,7 @@ import java.util.List;
 public class WebMvcConfig extends WebMvcConfigurationSupport {
     /**
      * 解决Long精度转化问题
+     *
      * @param converters
      */
     @Override
@@ -29,6 +31,21 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setObjectMapper(new JacksonObjectMapper());
 
-        converters.add(0,converter);
+        converters.add(0, converter);
+    }
+
+    /**
+     * 解决访问不到swagger ui界面
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
     }
 }
